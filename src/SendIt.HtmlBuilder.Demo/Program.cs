aefeff240a677c8efedf14533c7c3f029cf40830
Filip Liwiński <filipliwinski@users.noreply.github.com>
2021-07-19 00:32:02 +0200

    Base usage in ReadMe

--- src/SendIt.HtmlBuilder.Demo/Program.cs
+++ src/SendIt.HtmlBuilder.Demo/Program.cs
@@ -29,15 +29,79 @@ namespace SendIt.HtmlBuilder.Demo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            #region Define HTML tags as C# objects.
 
             var body = new Body();
-            body.AppendChild(new P("Hello ")).AppendChild(new P("World") { Id = "p-with-id" });
-            body.AppendChild(new Img("https://picsum.photos/400/300", width: 400, height: 300));
+
+            var h4 = new H4("This is a level 4 heading.");
+            var p = new P("This is a paragraph.");
+            var img = new Img("https://picsum.photos/400/300");
+
+            var table = new Table();
+            var tr1 = new TR();
+            var td1 = new TD("Cell 1");
+            var td2 = new TD("Cell 2");
+            var tr2 = new TR();
+            var td3 = new TD("Cell 3");
+            var td4 = new TD("Cell 4");
+
+            #endregion
+            #region Create HTML DOM.
+
+            tr1.AppendChild(td1);
+            tr1.AppendChild(td2);
+            tr2.AppendChild(td3);
+            tr2.AppendChild(td4);
+
+            table.AppendChild(tr1);
+            table.AppendChild(tr2);
+
+            body.AppendChild(h4);
+            body.AppendChild(p);
+            body.AppendChild(img);
+            body.AppendChild(table);
+
             var html = new Html(new Head(), body);
 
-            Console.WriteLine(html.ToHtml());
-            Console.WriteLine(html.ToText());
+            #endregion
+            #region Generate HTML string
+
+            var htmlString = html.ToHtml();
+            Console.WriteLine(htmlString);
+
+            #endregion
+            #region Use fluent methods
+
+            var htmlFluent = new Html(new Head(), new Body()
+                .AppendChild(new H4("This is a level 4 heading."))
+                .AppendChild(new P("This is a paragraph."))
+                .AppendChild(new Img("https://picsum.photos/400/300"))
+                .AppendChild(new Table()
+                    .AppendChild(new TR()
+                        .AppendChild(new TD("Cell 1"))
+                        .AppendChild(new TD("Cell 2")))
+                    .AppendChild(new TR()
+                        .AppendChild(new TD("Cell 3"))
+                        .AppendChild(new TD("Cell 4")))) as Body);
+
+            #endregion
+            #region Define attributes and styling
+
+            var h3 = new H3("This is a level 3 heading and it is blue")
+            {
+                Id = "Heading3",
+                Style = new Style()
+                    .Add(StyleProperty.Color, "blue")
+                    .Add(StyleProperty.FontFamily, "Arial")
+                    .Add(StyleProperty.FontSize, "0.9rem")
+            };
+
+            h3.Style[StyleProperty.FontWeight] = "600";
+
+            var h3String = h3.ToHtml();
+            Console.WriteLine(h3String);
+
+            #endregion
         }
     }
 }
